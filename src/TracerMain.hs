fd2a666a63662c7b369d67270c859e43dea32c39
Andrew Cowie <istathar@gmail.com>
2022-06-12 22:42:40 +1000

    Add command-line handling

--- src/TracerMain.hs
+++ src/TracerMain.hs
@@ -1,10 +1,55 @@
 {-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE QuasiQuotes #-}
+{-# LANGUAGE TemplateHaskell #-}
 {-# OPTIONS_GHC -fno-warn-unused-imports #-}
 
-module Main where
-
 import Core.Program
+import Core.Telemetry
+
+version :: Version
+version = $(fromPackage)
 
 main :: IO ()
-main = execute $ do
+main = do
+    context <-
+        configure
+            version
+            None
+            ( complexConfig
+                [ Global
+                    [ Option
+                        "trace-file"
+                        Nothing
+                        (Value "FILENAME")
+                        "File to record the trace and root span identifier in. The default filename is .trace in the current working directory."
+                    ]
+                , Command
+                    "init"
+                    "Create a trace and root span identifier and mark the start time of your trace."
+                    [ Argument
+                        "label"
+                        "Name for the root span."
+                    ]
+                , Command
+                    "leaf"
+                    "Wrap the execution of a command in a span, recording its start time and duration."
+                    [ Argument
+                        "label"
+                        "Name for the span that will enclose the command you are running."
+                    , Remaining "The command and arguments to run"
+                    ]
+                , Command
+                    "root"
+                    "Send the root span, finishing the corresponding trace."
+                    [ Argument
+                        "label"
+                        "Name for the root span."
+                    ]
+                ]
+            )
+    context' <- initializeTelemetry [consoleExporter, honeycombExporter] context
+    executeWith context' program
+
+program :: Program None ()
+program = do
     write "Hello World"
